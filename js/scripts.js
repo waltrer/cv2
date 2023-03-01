6f460463132250d3647e7661d5f23985f52364c5
waltrer <walterstelle@gmail.com>
2023-03-01 02:17:31 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 //Funcion adaptada del ejemplo de w3schools https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_read_more
 function verMasOMenos() {
-    var oculto = document.getElementById("mas");
-    var boton = document.getElementById("btn1");
+  var oculto = document.getElementById("mas");
+  var boton = document.getElementById("btn1");
 
-    if (mas.style.display === "none") {
-      oculto.style.display = "inline";
-      boton.textContent = "ver menos";
-    } else {
-      oculto.style.display = "none";
-      boton.textContent = "ver mas";
-    }
+  if (mas.style.display === "none") {
+    oculto.style.display = "inline";
+    boton.textContent = "ver menos";
+  } else {
+    oculto.style.display = "none";
+    boton.textContent = "ver mas";
   }
+}
