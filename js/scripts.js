2035cbd950f90dc8a89160dd4b37e5d4739c95f5
waltrer <walterstelle@gmail.com>
2023-03-02 17:24:43 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Funcion adaptada del ejemplo de w3schools https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_read_more
-function verMasOMenos(ocult,btn) {
-  var oculto = document.getElementById("ocult");
-  var boton = document.getElementById("btn");
+function verMasOMenos(ocult, btn) {
+  var oculto = document.getElementById(ocult);
+  var boton = document.getElementById(btn);
 
   if (mas.style.display === "none") {
     oculto.style.display = "inline";
